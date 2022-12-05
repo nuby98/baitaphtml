32bf0b71c16eb1793c7c3307cd91419bbc33f13b
nuby98 <nubypham@gmail.com>
2022-12-05 21:43:04 +0700

    first commit

---
+++ baitapss7/thuchanhss7.js
@@ -0,0 +1,32 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' +  f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+document.write('<br/>')
+// bai tap 2
+ let width = 20
+let height = 10
+let area = width * height
+document.write('area = ' + area)
+
+// bai tap 3
+document.write('<br/>')
+
+a = prompt("Nhập biến a: ");
+b = prompt("Nhập biến b: ");
+if (a%b == 0){
+    alert('a chia hết cho b');
+}
+else {
+    alert('a không chia hết cho b');
+}
